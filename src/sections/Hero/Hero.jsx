b5a7904909f6e60fae0b738afe2c64e18d923204
IngeSenekal <senekalinge@gmail.com>
2024-07-27 21:51:30 +0200

    Added personal details & link to LinkedIn

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/User.png";
 import themeIcon from "../../assets/sun.svg";
+import linkedInIcon from "../../assets/linkedin-light.svg";
 
 function Hero() {
   return (
@@ -16,6 +17,19 @@ function Hero() {
           src={themeIcon}
           alt="Color mode icon"
         />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Inge <br /> Senekal
+        </h1>
+
+        <h2>University Student</h2>
+
+        <span>
+          <a href="https://www.linkedin.com/in/ia-senekal" target="_blank">
+            <img src={linkedInIcon} alt="LinkedIn Icon" />
+          </a>
+        </span>
       </div>
     </section>
   );
