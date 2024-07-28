9da2000ebd17fd669e659f37d0005bac182636e2
IngeSenekal <senekalinge@gmail.com>
2024-07-28 10:31:36 +0200

    Added a theme
    Included new icons for different themes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,20 @@
 import styles from "./HeroStyles.module.css";
 import heroImg from "../../assets/User.png";
-import themeIcon from "../../assets/sun.svg";
-import linkedInIcon from "../../assets/linkedin-light.svg";
-import gitHubIcon from "../../assets/github-light.svg";
+import themeSun from "../../assets/sun.svg";
+import themeMoon from "../../assets/moon.svg";
+import linkedInIconLight from "../../assets/linkedin-light.svg";
+import gitHubIconLight from "../../assets/github-light.svg";
+import linkedInIconDark from "../../assets/linkedin-dark.svg";
+import gitHubIconDark from "../../assets/github-dark.svg";
+import { useTheme } from "../../common/ThemeContext";
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === "light" ? themeSun : themeMoon;
+  const linkedInIcon = theme === "light" ? linkedInIconLight : linkedInIconDark;
+  const gitHubIcon = theme === "light" ? gitHubIconLight : gitHubIconDark;
+
   return (
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
@@ -17,6 +27,7 @@ function Hero() {
           className={styles.colorMode}
           src={themeIcon}
           alt="Color mode icon"
+          onClick={toggleTheme}
         />
       </div>
       <div className={styles.info}>
