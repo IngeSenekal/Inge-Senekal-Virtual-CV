4cfb250493c2a138432fa85b2aea7f3eb234b4ee
IngeSenekal <senekalinge@gmail.com>
2024-07-28 20:13:51 +0200

    Added Project & Formatted Projects

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,30 @@
+import styles from "./ProjectsStyles.module.css";
+import ParadisePoint from "../../assets/Paradise Point.png";
+import OMT from "../../assets/OMT.png";
+import OMT2 from "../../assets/OMTvs2.png";
+import ProjectCard from "../../common/ProjectCard";
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={ParadisePoint}
+          link={"https://github.com/Theunis-Kok/Paradise_Point"}
+          h3="ParadisePoint"
+          p="System Design Project"
+        />
+
+        <ProjectCard
+          src={OMT2}
+          link={"https://github.com/IngeSenekal/OMT"}
+          h3="OMT(Optimise My Time)"
+          p="Scheduling Application"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
