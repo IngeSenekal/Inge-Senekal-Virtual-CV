52202999d61be8683a6b115e15e67009d9331dde
IngeSenekal <senekalinge@gmail.com>
2024-07-30 13:56:28 +0200

    Updated Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import SkillList from "../../common/SkillList";
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
-      <h1>Skills</h1>
+      <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkmarkIcon} skill="HTML" />
         <SkillList src={checkmarkIcon} skill="CSS" />
