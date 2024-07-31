42aeb1c0b041641f9c274a1c417d7a8bd23a8f18
IngeSenekal <senekalinge@gmail.com>
2024-07-31 18:48:34 +0200

    Formatting page

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,13 +5,21 @@ function About() {
     <section id="about" className={styles.container}>
       <div className={styles.info}>
         <h1>About</h1>
-        <p className={styles.description}>
-          I'm an innovative, highly motivated individual who values
-          self-discipline. I love leveraging data to drive informed
-          decision-making and streamline processes. Oh, and if you need someone
-          to debug a database at 2 AM, I'm your person—coffee in hand, of
-          course!
-        </p>
+        <div className={styles.description}>
+          <p>
+            Hello! I am a dedicated and passionate Information Technology
+            student at North-West University.
+          </p>
+          <p>
+            As a proactive learner, I'm constantly seeking new opportunities to
+            expand my knowledge and apply my skills in real-world scenarios.
+          </p>
+          <p>
+            I'm passionate about staying at the forefront of technological
+            advancements and am eager to contribute to innovative projects that
+            make a difference.
+          </p>
+        </div>
       </div>
     </section>
   );
